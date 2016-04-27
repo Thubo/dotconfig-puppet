81f4eee28dc86600de7444d7ebd6291fb1dc079c
Matthias Thubauville <matthias.thubo@gmail.com>
2016-04-27 19:50:30 +0200

    Example to checkout repositories via puppetlabs/vcsrepo

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,15 @@
 hiera_include('classes')
+
+class site::vcsrepos {
+
+  $vcsrepos = hiera_hash('vcsrepos')
+  create_resources (vcsrepo, $vcsrepos)
+
+}
+
+class site::symlinker {
+
+  $symlinks = hiera_hash('symlinks')
+  create_resources (file, $symlinks)
+
+}
