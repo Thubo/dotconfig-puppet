7060b13fd0be92e4268d0c1a620e1ff6b83b3328
Matthias Thubauville <matthias.thubo@gmail.com>
2016-05-24 18:21:21 +0200

    Some Cleanup

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,29 +1,13 @@
 hiera_include('classes')
 
-class site::vcsrepos {
 
-  $vcsrepos = hiera_hash('vcsrepos')
-  create_resources (vcsrepo, $vcsrepos)
+$packages        = hiera('site::packages')
+$symlinks        = hiera('symlinks')
+$vagrant_plugins = hiera('vagrant_plugins')
+$vcsrepos        = hiera('vcsrepos')
 
-}
+package { $packages: ensure => 'installed' }
 
-class site::symlinker {
-
-  $symlinks = hiera_hash('symlinks')
-  create_resources (file, $symlinks)
-
-}
-
-class site::vagrant_plugins {
-
-  $vagrant_plugins = hiera_hash('vagrant_plugins')
-  create_resources (vagrant::plugin, $vagrant_plugins)
-
-}
-
-class site::packages {
-
-  $enhancers = hiera('enhancers')
-  package { $enhancers: ensure => 'installed' }
-
-}
+create_resources (vcsrepo, $vcsrepos)
+create_resources (file, $symlinks)
+create_resources (vagrant::plugin, $vagrant_plugins)
