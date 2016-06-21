ba3559ad2375f9f05b9b20f99e10fbfb0d2f02f1
Matthias Thubauville <matthias.thubo@gmail.com>
2016-06-21 16:09:45 +0200

    Bugfix in librarian-puppet config

--- cron.sh
+++ cron.sh
@@ -1,4 +1,10 @@
+/usr/bin/logger "Starting local puppet cron job."
+
+/usr/bin/logger "Installing modules with librarian-puppet."
+librarian-puppet install --verbose
+
 /usr/bin/logger "Starting local puppet apply."
 puppet apply -t --pluginsync --hiera_config /etc/puppet/hiera.yaml --modulepath=/etc/puppet/modules/ /etc/puppet/manifests/site.pp
-/usr/bin/logger "Done with local puppet apply."
+
+/usr/bin/logger "Done with local puppet cron job."
 
